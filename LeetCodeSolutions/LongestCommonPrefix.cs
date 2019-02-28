74d3f835430414c95b4487ff83cf3be283cdc05e
rudi schenck <rudische@gmail.com>
2019-02-27 20:43:52 -0500

    Added problems with solutions

---
+++ LeetCodeSolutions/LongestCommonPrefix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    //Write a function to find the longest common prefix string amongst an array of strings.
+
+    //If there is no common prefix, return an empty string ""
+
+namespace LeetCodeSolutions
+{
+    class LongestCommonPrefix
+    {
+        public string LongestCommonPrefixes(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+            string prefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (String.IsNullOrEmpty(prefix))
+                    {
+                        return "";
+                    }
+                }
+            }
+            return prefix;
+        }
+    }
+}
---
+++ LeetCodeSolutions/TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+    //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+    //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+namespace LeetCodeSolutions
+{
+    class TwoSum
+    {
+        public int[] TwoSums(int[] nums, int target)
+        {
+            int[] twoSums = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if ((nums[i] + nums[j]) == target && i != j)
+                    {
+                        twoSums[0] = i;
+                        twoSums[1] = j;
+                        return twoSums;
+                    }
+                }
+            }
+
+            return twoSums;
+        }
+    }
+}
